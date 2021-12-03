b2bdb15557e22c36dec75b69bb6b8cd85342e29a
YermakA <ermak.080@mail.ru>
2021-12-04 02:52:12 +0800

    create: navbar-mobile-menu

--- js/main.js
+++ js/main.js
@@ -28,3 +28,9 @@ const reviewsSlider = new Swiper('.reviews-slider', {
 		onlyInViewport: false,
 	},
 });
+
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+	document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+})
